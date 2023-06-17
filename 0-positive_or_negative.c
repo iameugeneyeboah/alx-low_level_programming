c0827f49d96eed9623a4c7772bb37aa2fdfa9083
Eugene Yeboah <iameugene533@gmail.com>
2023-06-16 22:29:24 -0700

    positive and neg

--- 0-positive_or_negative.c
+++ 0-positive_or_negative.c
@@ -4,27 +4,20 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: o (success)
  */
 int main(void)
 {
-	int n, num;
-
+	int n;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	num = n % 10;
-	if (num > 5)
-	{
-	printf("Last digit of %d is %d and is greater than 5\n", n, num);
-	}
-	else if (num == 0)
-	{
-	printf("Last digit of %d is %d and is 0\n", n, num);
-	}
-	else if (num < 6)
-	{
-	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, num);
-	}
+	printf("%d ", n);
+	if (n == 0)
+		printf("is zero");
+	else if (n > 0)
+		printf("is positive");
+	else
+		printf("is negative");
+	printf("\n");
 	return (0);
 }
